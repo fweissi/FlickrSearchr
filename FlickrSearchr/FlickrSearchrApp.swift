7a9f1f3589c70cee7c45c971a358194d54cbbb46
Keith Weiss <kweiss@markettime.com>
2024-07-30 20:41:57 -0600

    Initial Commit

---
+++ FlickrSearchr/FlickrSearchrApp.swift
@@ -0,0 +1,32 @@
+//
+//  FlickrSearchrApp.swift
+//  FlickrSearchr
+//
+//  Created by Keith Weiss on 7/30/24.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct FlickrSearchrApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
